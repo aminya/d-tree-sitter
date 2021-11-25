dcda4445bb79ef8a6bf9dfac551819ae35abbf20
JEphron <jephron63@gmail.com>
2021-11-24 20:12:55 -0800

    Add documentation

--- src/d_tree_sitter/query.d
+++ src/d_tree_sitter/query.d
@@ -64,11 +64,19 @@ struct QueryIterator
         ts_query_cursor_delete(cursor);
     }
 
+    /**
+     * Adjusts the range in which the query will apply.
+     * `min` and `max` are byte offsets.
+     */
     void set_byte_range(uint min, uint max)
     {
         ts_query_cursor_set_byte_range(cursor, min, max);
     }
 
+    /**
+     * Adjusts the range in which the query will apply.
+     * `min` and `max` are Point offsets.
+     */
     void set_point_range(Point min, Point max)
     {
         ts_query_cursor_set_point_range(cursor, min, max);
@@ -137,41 +145,90 @@ struct Query
         ts_query_delete(tsquery);
     }
 
+    /**
+     * Execute a query over an entire node.
+     *
+     * The caller may iterate over the result to recieve a series of
+     * `QueryMatch` results.
+     */
     QueryIterator exec(Node node)
     {
         return QueryIterator(&this, &node);
     }
 
+    /**
+     * Execute a query between given start and end byte offsets.
+     *
+     * The caller may iterate over the result to recieve a series of
+     * `QueryMatch` results.
+     */
     QueryIterator exec(Node node, uint min, uint max)
     {
         return QueryIterator(&this, &node, min, max);
     }
 
+    /**
+     * Execute a query between given start and end `Points`.
+     *
+     * The caller may iterate over the result to recieve a series of
+     * `QueryMatch` results.
+     */
     QueryIterator exec(Node node, Point min, Point max)
     {
         return QueryIterator(&this, &node, min, max);
     }
 
+    /**
+     * Get the number of patterns in the query.
+     */
     int pattern_count() @nogc nothrow
     {
         return ts_query_pattern_count(tsquery);
     }
 
+    /**
+     * Get the number of captures in the query.
+     */
     int capture_count() @nogc nothrow
     {
         return ts_query_capture_count(tsquery);
     }
 
+    /**
+     * Get the number of string literals in the query.
+     */
     int string_count() @nogc nothrow
     {
         return ts_query_string_count(tsquery);
     }
 
+    /**
+     * Get the byte offset where the given pattern starts in the query's source.
+     *
+     * This can be useful when combining queries by concatenating their source
+     * code strings.
+     */
     int start_byte_for_pattern(uint patternId) @nogc nothrow
     {
         return ts_query_start_byte_for_pattern(tsquery, patternId);
     }
 
+    /**
+     * Get all of the predicates for the given pattern in the query.
+     *
+     * The predicates are represented as a single array of steps. There are three
+     * types of steps in this array, which correspond to the three legal values for
+     * the `type` field:
+     * - `TSQueryPredicateStepTypeCapture` - Steps with this type represent names
+     *    of captures. Their `value_id` can be used with the
+     *   `ts_query_capture_name_for_id` function to obtain the name of the capture.
+     * - `TSQueryPredicateStepTypeString` - Steps with this type represent literal
+     *    strings. Their `value_id` can be used with the
+     *    `ts_query_string_value_for_id` function to obtain their string value.
+     * - `TSQueryPredicateStepTypeDone` - Steps with this type are *sentinels*
+     *    that represent the end of an individual predicate. If a pattern has two
+     *    predicates, then there will be two steps with this `type` in the array.
+     */
     const(TSQueryPredicateStep)[] predicates_for_pattern(uint patternId) @nogc nothrow
     {
         uint len;
@@ -179,11 +236,23 @@ struct Query
         return ptr[0 .. len];
     }
 
+    /*
+     * Check if a given step in a query is 'definite'.
+     *
+     * A query step is 'definite' if its parent pattern will be guaranteed to match
+     * successfully once it reaches the step.
+     */
     bool step_is_definite(uint byteOffset) @nogc nothrow
     {
         return ts_query_step_is_definite(tsquery, byteOffset);
     }
 
+    /**
+     * Get the name of one of the query's captures.
+     *
+     * Each capture is associated with a numeric id based on the order that it
+     * appeared in the query's source.
+     */
     string capture_name_for_id(uint captureId) nothrow
     {
         uint len;
@@ -191,11 +260,19 @@ struct Query
         return namePtr[0 .. len].to!string;
     }
 
+    /**
+     * Get the name of one of the query's captures, given a TSQueryCapture.
+     */
     string capture_name(TSQueryCapture capture) nothrow
     {
         return capture_name_for_id(capture.index);
     }
-
+    /*
+     * Get the name of one of the query's string literals.
+     *
+     * Each string is associated with a numeric id based on the order that it
+     * appeared in the query's source.
+     */
     string query_string_value_for_id(uint id) nothrow
     {
         uint len;
@@ -203,11 +280,24 @@ struct Query
         return namePtr[0 .. len].to!string;
     }
 
+    /**
+     * Disable a certain capture within a query.
+     *
+     * This prevents the capture from being returned in matches, and also avoids
+     * any resource usage associated with recording the capture. Currently, there
+     * is no way to undo this.
+     */
     void disable_capture(string captureName)
     {
         ts_query_disable_capture(tsquery, captureName.toStringz, captureName.length.to!uint);
     }
 
+    /**
+     * Disable a certain pattern within a query.
+     *
+     * This prevents the pattern from matching and removes most of the overhead
+     * associated with the pattern. Currently, there is no way to undo this.
+     */
     void disable_pattern(uint patternId) @nogc nothrow
     {
         ts_query_disable_pattern(tsquery, patternId);
