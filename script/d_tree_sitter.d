e32d6f2f26de06204c8c13d4646c26dd07a7c153
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-12-12 05:41:40 -0600

    build: fix build on macos

--- script/d_tree_sitter.d
+++ script/d_tree_sitter.d
@@ -42,8 +42,16 @@ void bind_tree_sitter(string treeSitterPackageDir, string treeSitterDlDir,
   const prebuiltHeaders = headersMap.byKeyValue()
     .map!((elm) => (" --prebuilt-header " ~ elm.key ~ "=" ~ elm.value))().join(" ");
 
-  // TODO macos fails to find stbool.h
-  const headers = " --ignore-system-paths --no-sys-headers " ~ prebuiltHeaders;
+  string headers;
+  version (linux)
+  {
+    headers = " --ignore-system-paths --no-sys-headers " ~ prebuiltHeaders;
+  }
+  else
+  {
+    headers = " --ignore-system-paths " ~ prebuiltHeaders;
+  }
+
   executeShellAt(dppBin ~ headers ~ " --keep-d-files " ~ treeSitterLibcDpp
       ~ " --include-path " ~ treeSitterIncludeDir ~ " --source-output-path "
       ~ treeSitterPackageDir ~ " --preprocess-only --compiler=" ~ compiler, treeSitterGenDir);
